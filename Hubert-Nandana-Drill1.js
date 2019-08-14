9cb2c6056aa6c54cf3180a4cab2697b3df9f11cd
nrrao <nandanarao@yahoo.com>
2019-08-14 12:33:16 -0400

    delete key

--- Hubert-Nandana-Drill1.js
+++ Hubert-Nandana-Drill1.js
@@ -39,3 +39,20 @@ function personMaker() {
 let persons = personMaker();
 
 console.log(persons.fullName());
+
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  // your code here
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+console.log(keyDeleter(sampleObj));
