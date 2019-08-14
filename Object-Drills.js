4fd5d31ab56b59b7cd3f69198b361a7bd4998248
nrrao <nandanarao@yahoo.com>
2019-08-14 15:06:34 -0400

    problem 3

--- Object-Drills.js
+++ Object-Drills.js
@@ -24,14 +24,23 @@ let object={
   spam:'sandwitch'
 
 };
- for(let key in object){
-     console.log(`${key} : ${object[key]}`);
- }
+for(let key in object){
+  console.log(`${key} : ${object[key]}`);
+}
 
- // 3. Array in Objects
+// 3. Array in Objects
 
- let food = {
-     meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
- };
+let food = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
 
- console.log(food.meals[3]);
+console.log(food.meals[3]);
+
+let jobs=[{name:'John',jobTitle:'sales'},
+  {name:'Alex',jobTitle:'teacher'},
+  {name:'Nandana',jobTitle:'developer'},
+  {name:'Hubert',jobTitle:'developer'}];
+
+  for (let i=0;i<jobs.length;i++){
+      console.log(`${jobs[i].jobTitle}:${jobs[i].name}`);
+  }
