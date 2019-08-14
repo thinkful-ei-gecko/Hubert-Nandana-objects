577356d3723393abe3e9f6095ec8b42d299d0daf
nrrao <nandanarao@yahoo.com>
2019-08-14 14:55:05 -0400

    2nd problem

--- Object-Drills.js
+++ Object-Drills.js
@@ -1,16 +1,29 @@
 // OBJECT DRILLS
 
 // 1. Object initializers and methods
+'use strict';
 
 function loaf() {
-    return {
-        flour: 300,
-        water: 210,
-        hydration() {
-            return (this.water / this.flour) * 100;
-        }
+  return {
+    flour: 300,
+    water: 210,
+    hydration() {
+      return (this.water / this.flour) * 100;
     }
+  }
 }
 
 let bread = loaf();
 console.log(bread.hydration());
+
+let object={
+  foo:'happy',
+  bar:'chcolate',
+  fum:'icecream',
+  quux:'soda',
+  spam:'sandwitch'
+
+};
+ for(let key in object){
+     console.log(`${key} : ${object[key]}`);
+ }
