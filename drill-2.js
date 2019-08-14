3e40b5929952316defa4ef340acf07838c4d7662
Hubert Yang <offspring7890@yahoo.com>
2019-08-14 11:17:28 -0700

    finish find ID drill

--- drill-2.js
+++ drill-2.js
@@ -1,49 +1,70 @@
 // Object Drills - 2
-'use strict';
 
-// Make Student Reports
-function makeStudentsReport(data) {
-  let arr = [];
-  for (let i = 0; i < data.length; i++) {
-    arr.push(`${data[i].name}: ${data[i].grade}`);
-  }
-  return arr;
-}
 
-const test = [{
-  name: 'Johnny Robot', 
-  grade: 'C'
-}];
+// Make Student
+// function makeStudentsReport(data) {
+//     let arr = [];
+//     for (let i = 0; i < data.length; i++) {
+//         arr.push(`${data[i].name}: ${data[i].grade}`);
+//     }
+//     return arr
+//   }
 
-let dataz = makeStudentsReport(test);
-console.log(dataz);
+// const test = [{
+//     name: 'Johnny Robot', 
+//   name: 'Johnny Robot', 
+//     name: 'Johnny Robot', 
+//     grade: 'C'
+// }];
 
-const studentData = [
-  {
-    name: 'Tim',
-    status: 'Current student',
-    course: 'Biology',
-  },
-  {
-    name: 'Sue',
-    status: 'Withdrawn',
-    course: 'Mathematics',
-  },
-  {
-    name: 'Liz',
-    status: 'On leave',
-    course: 'Computer science',
-  },
+// let dataz = makeStudentsReport(test);
+// console.log(dataz);
+
+// Find by ID
+
+// you can pass in `scratchData` to test out `findByid`
+// your function
+const scratchData = [
+  { id: 22, foo: 'bar' },
+  { id: 28, foo: 'bizz' },
+  { id: 19, foo: 'bazz' },
 ];
-  
-function enrollInSummerSchool(students) {
+
+function findById(items, idNum) {
   // your code here
-  let result=[];
-  for (let i=0; i<students.length;i++){
-      result.push({name:students[i].name,
-      status:'In Summer School',
-      course:students[i].course})
+  for (let i=0; i < items.length; i++) {
+    if (items[i].id === idNum) {
+      return items[i];
+    }
   }
-  return result;
 }
-console.log(enrollInSummerSchool(studentData));
+
+//
+
+function testIt() {
+  const testData = [
+    { id: 1, foo: 'bar' },
+    { id: 2, foo: 'bizz' },
+    { id: 3, bang: 'boo' },
+  ];
+  const result = findById(testData, 3);
+  if (!(result && result !== null && typeof result === 'object')) {
+    console.error('`findById` must return an object');
+    return;
+  }
+  if (result.id !== 3) {
+    console.error(
+      'Asked for item with id of `3` but got back one with id of ' + result.id
+    );
+    return;
+  }
+  if (result.bang !== 'boo') {
+    console.error(
+      'Expected all key/value pairs from target object to be returned'
+    );
+    return;
+  }
+  console.log('SUCCESS: `findByid` is working');
+}
+
+testIt();
