acad85e51642014faec44c6a26173d972b251e84
nrrao <nandanarao@yahoo.com>
2019-08-14 14:10:16 -0400

    enroll student data

--- drill-2.js
+++ drill-2.js
@@ -1,19 +1,49 @@
 // Object Drills - 2
-
+'use strict';
 
 // Make Student Reports
 function makeStudentsReport(data) {
-    let arr = [];
-    for (let i = 0; i < data.length; i++) {
-        arr.push(`${data[i].name}: ${data[i].grade}`);
-    }
-    return arr
+  let arr = [];
+  for (let i = 0; i < data.length; i++) {
+    arr.push(`${data[i].name}: ${data[i].grade}`);
   }
+  return arr;
+}
 
 const test = [{
-    name: 'Johnny Robot', 
-    grade: 'C'
+  name: 'Johnny Robot', 
+  grade: 'C'
 }];
 
 let dataz = makeStudentsReport(test);
 console.log(dataz);
+
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+  
+function enrollInSummerSchool(students) {
+  // your code here
+  let result=[];
+  for (let i=0; i<students.length;i++){
+      result.push({name:students[i].name,
+      status:'In Summer School',
+      course:students[i].course})
+  }
+  return result;
+}
+console.log(enrollInSummerSchool(studentData));
