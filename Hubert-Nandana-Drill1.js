e5cb0095eaf004c6796ad5856837ea7988a8a4e6
nrrao <nandanarao@yahoo.com>
2019-08-14 12:25:08 -0400

    update object

--- Hubert-Nandana-Drill1.js
+++ Hubert-Nandana-Drill1.js
@@ -1,13 +1,27 @@
-function createMyObject() {
-    return {
-        foo: 'bar',
-        answerToUniverse: 42,
-        'olly olly': 'oxen free',
-        sayHello() {
-            console.log('hello');
-        }
-    };
+/* eslint-disable strict */
+/*function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello() {
+      console.log('hello');
+    }
+  };
 }
 
 let sayHi = createMyObject();
-console.log(sayHi.sayHello());
+console.log(sayHi.sayHello());*/
+
+function updateObject(obj){
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz='bizz';
+  obj.bang='bang';
+  return obj;
+}
+
+let obj1={};
+let updateObj =updateObject(obj1);
+console.log(updateObj);
+
